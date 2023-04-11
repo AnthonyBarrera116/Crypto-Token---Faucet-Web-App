04454250c19663a9aacff48689e2c76c29c809a8
Anthony Barrera <55001338+AnthonyBarrera116@users.noreply.github.com>
2023-04-11 09:39:14 -0400

    Add files via upload

--- college-token/contracts/collegeToken.sol
+++ college-token/contracts/collegeToken.sol
@@ -2,41 +2,58 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+// Import of openZepplion
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// Contract Creation for College Token with ERC20 node modules
 contract CollegeToken is ERC20Capped,ERC20Burnable{
 
+    // Owner of contract
     address payable public owner;
 
+    // Block Reware in this case doesn't work since it is on test net
     uint256 public blockReward;
 
-    // decimals()
+    // Mint the owner of the contract the max copacity of the token
     constructor (uint cap, uint256 reward) ERC20("CollegeToken","CT") ERC20Capped(cap * (10 ** 18)){
 
+        // Making owner payable
         owner = payable(msg.sender);
+
+        // minting
         _mint(owner, 70000000 * (10 ** 18));
+        
+        // Blcok reward is the reward of the decimals of value
         blockReward =  reward * (10 * 18);
 
     }
 
+    // Minting coins 
     function _mint(address account, uint256 amount) internal virtual override(ERC20Capped, ERC20) {
+        
+        // making sure there is a capped ERC20 of ERC20 Cap
         require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
+
+        // Mints ammount to owner
         super._mint(account, amount);
     }
 
+    // Mint reward to Miner
     function _mintMinerReward() internal{
 
+        // Miner and coin mint
         _mint(block.coinbase, blockReward);
 
     }
 
+    // Before transfering coin to address making sure address is are vaild 
     function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override{
 
         if (from != address(0) && to != block.coinbase && block.coinbase != address(0)){
 
-            
+            // mint reward to miner
             _mintMinerReward();
 
         }
@@ -44,18 +61,24 @@ contract CollegeToken is ERC20Capped,ERC20Burnable{
 
     }
 
+
+    // Set block reward
     function setBlockReward(uint256 reward) public onlyOwner{
         
+        // block reward
         blockReward = reward * (10 * 18);
 
     }
 
+    // never used but found out it's good practice if needed to 
     function destroy() public onlyOwner{
 
+        // Self destructs contract
         selfdestruct(owner);
 
     }
 
+    // only owner modfier 
     modifier onlyOwner{
 
         require(msg.sender == owner, "Only Owner can change reward");
--- college-token/contracts/fauc.sol
+++ college-token/contracts/fauc.sol
@@ -3,67 +3,108 @@
 
 pragma solidity ^0.8.17;
 
+// interface for ERC20 
 interface IERC20 {
+
+    // transfer ammount to account from faucet
     function transfer(address to, uint256 amount) external returns (bool);
 
+    // Balance of account
     function balanceOf(address account) external view returns (uint256);
 
+    // transfer from faucet to address
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
+
+// faucet contract
 contract Faucet {
+
+    // Contract owner
     address payable owner;
+
+    // Token interface which is for faucet
     IERC20 public token;
 
+    // Withdrawl amount 50 
     uint256 public withdrawalAmount = 50 * (10**18);
+    
+    // Lock address for 1 minute and can mint again
     uint256 public lockTime = 1 minutes;
 
+    // withdrawl to account
     event Withdrawal(address indexed to, uint256 indexed amount);
+    
+    // Depositing from account
     event Deposit(address indexed from, uint256 indexed amount);
 
+    // For access time next time you can accesss
     mapping(address => uint256) nextAccessTime;
 
+    // payable costruter hold address and IERC20 token address 
     constructor(address tokenAddress) payable {
         token = IERC20(tokenAddress);
         owner = payable(msg.sender);
     }
 
+    // When person requests token from website 
     function requestTokens() public {
+        
+        // address id not the same 
         require(
             msg.sender != address(0),
             "Request must not originate from a zero account"
         );
+
+        // checking for insufficent funds for gas
         require(
             token.balanceOf(address(this)) >= withdrawalAmount,
             "Insufficient balance in faucet for withdrawal request"
         );
+
+        // Time break
         require(
             block.timestamp >= nextAccessTime[msg.sender],
             "Insufficient time elapsed since last withdrawal - try again later."
         );
-
+        
+        // Writes time if msg sender in map
         nextAccessTime[msg.sender] = block.timestamp + lockTime;
 
+        // Transfers tokens
         token.transfer(msg.sender, withdrawalAmount);
     }
 
+    // recieve for contract account that holds all needs to send tokens to facuet
     receive() external payable {
+
         emit Deposit(msg.sender, msg.value);
+
     }
 
+    // Withdrawl amount set if want to change
     function setWithdrawalAmount(uint256 amount) public onlyOwner {
+
         withdrawalAmount = amount * (10**18);
+
     }
 
+    // chnage lock time
     function setLockTime(uint256 amount) public onlyOwner {
+
         lockTime = amount * 1 minutes;
+
     }
 
+    // withdrawl is vaild for owner
     function withdraw() external onlyOwner {
+
         emit Withdrawal(msg.sender, token.balanceOf(address(this)));
+        
         token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
+    // Modifier owner
     modifier onlyOwner() {
         require(
             msg.sender == owner,
